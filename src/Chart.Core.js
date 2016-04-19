ea500b13b3bf236188d3b1a8a8baa8ca1d8ef580
hbrls <shuaizhexu@gmail.com>
2016-04-19 11:45:47 +0800

    remove xLabelRotation

--- src/Chart.Core.js
+++ src/Chart.Core.js
@@ -1663,27 +1663,28 @@
 				//Allow 3 pixels x2 padding either side for label readability
 				var xGridWidth = Math.floor(this.calculateX(1) - this.calculateX(0)) - 6;
 
-				//Max label rotate should be 90 - also act as a loop counter
-				while ((this.xLabelWidth > xGridWidth && this.xLabelRotation === 0) || (this.xLabelWidth > xGridWidth && this.xLabelRotation <= 90 && this.xLabelRotation > 0)){
-					cosRotation = Math.cos(toRadians(this.xLabelRotation));
+				// //Max label rotate should be 90 - also act as a loop counter
+				// while ((this.xLabelWidth > xGridWidth && this.xLabelRotation === 0)
+				// 	  || (this.xLabelWidth > xGridWidth && this.xLabelRotation <= 90 && this.xLabelRotation > 0)) {
+				// 	cosRotation = Math.cos(toRadians(this.xLabelRotation));
 
-					firstRotated = cosRotation * firstWidth;
-					lastRotated = cosRotation * lastWidth;
+				// 	firstRotated = cosRotation * firstWidth;
+				// 	lastRotated = cosRotation * lastWidth;
 
-					// We're right aligning the text now.
-					if (firstRotated + this.fontSize / 2 > this.yLabelWidth){
-						this.xScalePaddingLeft = firstRotated + this.fontSize / 2;
-					}
-					this.xScalePaddingRight = this.fontSize/2;
+				// 	// We're right aligning the text now.
+				// 	if (firstRotated + this.fontSize / 2 > this.yLabelWidth){
+				// 		this.xScalePaddingLeft = firstRotated + this.fontSize / 2;
+				// 	}
+				// 	this.xScalePaddingRight = this.fontSize/2;
 
 
-					this.xLabelRotation++;
-					this.xLabelWidth = cosRotation * originalLabelWidth;
+				// 	this.xLabelRotation++;
+				// 	this.xLabelWidth = cosRotation * originalLabelWidth;
 
-				}
-				if (this.xLabelRotation > 0){
-					this.endPoint -= Math.sin(toRadians(this.xLabelRotation))*originalLabelWidth + 3;
-				}
+				// }
+				// if (this.xLabelRotation > 0){
+				// 	this.endPoint -= Math.sin(toRadians(this.xLabelRotation))*originalLabelWidth + 3;
+				// }
 			}
 			else{
 				this.xLabelWidth = 0;
